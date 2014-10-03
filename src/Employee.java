00cf89be263cb938e59c38f452c39c95fa0c51d4
jere1330 <jere1330@ISS147D8-04.scdsb.pri>
2014-10-03 09:18:05 -0400

    Worked on the employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,6 +1,83 @@
 public class Employee {
-
-        private int type;
-        private double hoursworked;
-        
+    private String name;
+    private int type;
+    private double rate;
+    private int hours;
+    
+    public Employee(){
+        name = "";
+        type = 0;
+        rate = 0;
+        hours = 0;
+    }
+    
+    public String getTypeRules(){
+        return "1 or 2";
+    }
+    
+    public String getNameRules(){
+        return "nonblank";
+    }
+    
+    public String getRateRules(){
+        return "between 6.75 and 30.50, inclusive";
+    }
+    
+    public String getHourRules(){
+        return "between 1 and 60, inclusive";
+    }
+    
+    public boolean setName(String nm){
+        boolean blank = (nm.equals(""));
+        if(blank)
+            return false;
+        else{
+            name = nm;
+            return true;
+        }
+    }
+    
+    public boolean setType(int tp){
+        boolean oneortwo = (tp == 1 || tp == 2);
+        if (oneortwo){
+            type = tp;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public boolean setRate(double rt){
+        boolean rateok = (rt >= 6.75 && rt <= 30.50);
+        if(rateok){
+            rate = rt;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public boolean setHours(int hrs){
+        boolean hoursok = (hrs >= 1 && hrs <= 60);
+        if (hoursok){
+            hours = hrs;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public double getPay(){
+        double pay;
+        boolean noovertime = (hours <= 40 || type == 2);
+        if (noovertime)
+            pay = rate *hours;
+        else
+            pay = (hours-40)*(rate * 2) + rate * 40;
+        return pay;
+    }
 }
--- src/MakeEmployees.java
+++ src/MakeEmployees.java
@@ -3,10 +3,43 @@ import TerminalIO.*;
 public class MakeEmployees {
 
     public static void main(String[] args) {
-        KeyboardReader kr = new KeyboardReader();
-        String name = kr.readLine("Enter your name: ");
-        System.out.println("Hello " + name);
-        System.out.println("This is another line");
-    }
+        KeyboardReader r = new KeyboardReader();
+        Employee emp; //employee
+        String name; //name
+        int type; //type
+        double rate; //hourly pay rate
+        int hours; //hours worked
+        String prompt; //user prompt
+        emp = new Employee();
+        while (true){
+            //get the name and break if blank
+            System.out.println("Enter employee data");
+            name = r.readLine("Name (or <enter> to quit):");
+            if(!(emp.setName(name)))break;
+            //get the type until valid
+            while (true){
+                prompt = " Type (" + emp.getTypeRules() + "): ";
+                type = r.readInt(prompt);
+                if(emp.setType(type))break;
+            }
+            //get the hourly pay rate until valid
+            while(true){
+                prompt = " Hourly rate(" + emp.getRateRules()+ "): ";
+                rate = r.readDouble(prompt);
+                if(emp.setRate(rate)) break;
+            }
+            //get the hours worked until valid
+            while(true){
+                prompt = " Hours(" + emp.getHourRules() + "): ";
+                hours = r.readInt(prompt);
+                if(emp.setHours(hours))break;
+            }
+            //print the name and pay
+            System.out.print(" The weekly pay for ");
+            System.out.print(emp.getName());
+            System.out.println(" is $" + emp.getPay());
+            
+        }//end larger while       
+    }//end main
     
 }
