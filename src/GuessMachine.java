eb303f46292824a0191020862a805c10a5d4a8f0
jere1330 <jere1330@ISS147D8-04.scdsb.pri>
2014-10-07 09:04:51 -0400

    Guessing game is complete

--- src/GuessMachine.java
+++ src/GuessMachine.java
@@ -7,11 +7,33 @@ public class GuessMachine {
     //constructor
     public GuessMachine(){
         //generates random number from 1 to 100
-        number = (int)(Math.random()*100)+1; 
+        number = (int)(Math.floor(Math.random()*100))+1; 
         guess = 0;
         numguesses = 0;
     }
     
-    //to do - add methods:
-    //giveHint(), setGuess(), getNumGuesses() 
+    //was it higher or lower
+    public String giveHint(){
+        if(guess > number)
+            return "Your guess was greater than the number";
+        else if(guess < number)
+            return "Your guess was less than the number";
+        else
+            return "You got it!";
+    }
+    //was it valid?
+    public boolean setGuess(int valid){
+        if(valid >=1 && valid <= 100){
+            numguesses++;
+            guess = valid;
+            return true;
+        }
+        else
+            return false;
+    }
+    //add number of guesses
+    public int getNumGuesses(){
+        return numguesses;
+    }
+    
 }
