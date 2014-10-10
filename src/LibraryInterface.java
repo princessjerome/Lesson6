75183ae84a578d68882ab44da484ad7ce7330eb3
jere1330 <jere1330@ISS147D8-04.scdsb.pri>
2014-10-10 08:10:06 -0400

    Finish library class

--- src/LibraryInterface.java
+++ src/LibraryInterface.java
@@ -8,11 +8,11 @@ public class LibraryInterface {
         b4 = new Book("Good Housekeeping", "Pat Burns");
         b5 = new Book("To Be a Model", "Lisa Lahey");
         System.out.println("Here are the books available: ");
-        System.out.println(b1);
-        System.out.println(b2);
-        System.out.println(b3);
-        System.out.println(b4);
-        System.out.println(b5);
+        System.out.println(b1.getTitle());
+        System.out.println(b2.getTitle());
+        System.out.println(b3.getTitle());
+        System.out.println(b4.getTitle());
+        System.out.println(b5.getTitle());
         Patron p = new Patron("Miles Davis");
         
         System.out.println("Lending 4 books to " + p.getName());
@@ -28,5 +28,14 @@ public class LibraryInterface {
         if(p.borrow(b4)) System.out.println (b4.getTitle() + " successfully borrowed.");
         else             System.out.println (b4.getTitle() + " could not be borrowed.");
         
+        System.out.println("------\nAttempting to return the first book:\n---------");
+        if(p.returnBook(b1)) System.out.println(b1.getTitle() + " successfully returned.");
+        else                 System.out.println(b1.getTitle() + " was not borrowed out.");
+        System.out.println("------\nHere is a list of books currently lent to " + p.getName() + "\n---------");
+        if (p.hasBook(b1)) System.out.println(b1.getTitle());
+        if (p.hasBook(b2)) System.out.println(b2.getTitle());
+        if (p.hasBook(b3)) System.out.println(b3.getTitle());
+        if (p.hasBook(b4)) System.out.println(b4.getTitle());
+        if (p.hasBook(b5)) System.out.println(b5.getTitle());
     }
 }
